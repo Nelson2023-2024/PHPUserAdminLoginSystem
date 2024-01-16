3e52d23100d614b70263d27a23b64b7f4077da74
Nelson <nelsonobuya19@gmail.com>
2024-01-16 12:55:39 +0300

    Added SESSION for access by only the ADMINS

--- Part2/admin-view-users.php
+++ Part2/admin-view-users.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['user_level']) or $_SESSION['user_level'] !==1){
+    header("Location: login.php");
+    exit();
+}
+
+var_dump($_SESSION['user_level'])
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
